2ebc8401179f15422ffa62f460b9da5631294911
Inder Singh <inder.patliya@LM-ILB-INDER.local>
2019-01-02 19:18:14 +0530

    Updates

---
+++ precache-manifest.8cc099baabdf9b71561ec67f752ed146.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "56a1c76050ce3f023e30",
+    "url": "/portfolio/static/js/main.56a1c760.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/portfolio/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "56a1c76050ce3f023e30",
+    "url": "/portfolio/static/css/main.e77e5187.chunk.css"
+  },
+  {
+    "revision": "8998c57efdc0e5d4bfe08ea875d7d531",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c151176ce0d5fbac730ca576d71e15d0.js"
+  "/portfolio/precache-manifest.8cc099baabdf9b71561ec67f752ed146.js"
 );
 
 workbox.clientsClaim();
